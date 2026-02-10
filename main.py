ff3952892219d8ee9b8027fb565299f23f525845
Soudamini <93474039+soudaminigit@users.noreply.github.com>
2026-02-10 11:07:13 +0530

    Fixing the model loading path

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 
-with open("models/model.pkl", "rb") as f:
+with open("./models/model.pkl", "rb") as f:
     model = pickle.load(f)
 
-with open("models/features.pkl", "rb") as f:
+with open("./models/features.pkl", "rb") as f:
     features = pickle.load(f)
 
 app = FastAPI()
@@ -25,3 +25,4 @@ def predict(p: Passenger):
     X = [[p.Pclass, p.Sex, p.Age, p.SibSp, p.Parch, p.Fare]]
     y_pred = model.predict(X)[0]
     return {"survived": int(y_pred)}
+
